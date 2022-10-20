709c66ac745dbd3cc485321574ce14dfc04f99c6
Pete Hall <wasadmin@webagesolutions.com>
2022-10-20 14:25:12 -0400

    Added quick comment

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,17 +1,18 @@
 function Timer(props) {
   return (
-    <div
-      style={{
-        margin: '0px auto',
-        width: '500px',
-        borderRadius: '10px',
-        backgroundColor: 'rgba(0,0,0,.1)',
-        textAlign: 'center',
-      }}
-    >
-      <p style={{ fontSize: '3em', paddingTop: '15px' }}>{props.currentTime}</p>
-    </div>
-  );
+		<div
+			style={{
+				margin: "0px auto",
+				width: "500px",
+				borderRadius: "10px",
+				backgroundColor: "rgba(0,0,0,.1)",
+				textAlign: "center",
+			}}
+		>
+			{/* render the current Time decided from the Configure input */}
+			<p style={{ fontSize: "3em", paddingTop: "15px" }}>{props.currentTime}</p>
+		</div>
+	);
 }
 
 export default Timer;
